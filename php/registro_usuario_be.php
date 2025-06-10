80de1b82513b997ea46ea51b05b8489dd4f9236c
Joaquin <joacoparedes2204@gmail.com>
2025-06-10 11:21:42 -0300

    registro de usuarios

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -11,14 +11,54 @@
     $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena)
              VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
     
-    $ejecutar = mysqli_query($conexion, $query);
+     //verificar que el correo no se repita en la base de datos
+     $verificar_correo=mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' ");
+
+     if(mysqli_num_rows($verificar_correo) > 0){
+        echo '
+            <script>
+                alert("Este correo ya esta registrado, intenta con otro diferente");
+                window.location="../index.php";
+            </script>
+        ';
+        exit();
+
+     }
+     
+
+     //verificar que el nombre de usuario no se repita en la base de datos
+     $verificar_usuario=mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario' ");
+
+     if(mysqli_num_rows($verificar_correo) > 0){
+        echo '
+            <script>
+                alert("Este usuario ya esta registrado, intenta con otro diferente");
+                window.location="../index.php";
+            </script>
+        ';
+        exit();
+
+     }
+
+
+
+     $ejecutar = mysqli_query($conexion, $query);
 
     if($ejecutar){
         echo'
             <script>
                 alert("usuario almacenado exitosamente");
-                
+                window.location= "../index.php";
+            </script>
+        ';
+    }else{
+        echo'
+            <script>
+                alert("intentalo de nuevo, usuario no almacenado");
+                window.location= "../index.php";
             </script>
         ';
     }
+
+    mysqli_close($conexion);
 ?>
